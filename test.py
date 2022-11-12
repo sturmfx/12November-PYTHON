f6544cccec4298c9e18fd3cb3e22d8f440d07d86
sturmfx <ttunikitashtobert@gmail.com>
2022-11-12 17:09:05 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
+import turtle
+t = turtle.Turtle()
+t.pencolor("red")
 
+def figure(edges, length):
+    a = round(length/edges)
+    b = 5
+    c = round(a/(2*b))
+    degree = round(360/edges)
+    for i in range(edges):
+        for j in range(c):
+            t.forward(b)
+            t.penup()
+            t.forward(b)
+            t.pendown()
+        t.right(degree)
+
+figure(7, 1000)
